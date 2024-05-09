fffb3a9952915c04356115e0d16ab23f69f8bba6
Mala Ruparel <mala.ruparel@smartsensesolutions.com>
2024-05-09 14:36:26 +0530

    #correct file name

--- ss-smart-filter/src/main/java/com/ss/smartfilterlib/utils/Params.kt
+++ ss-smart-filter/src/main/java/com/ss/smartfilterlib/utils/Params.kt
@@ -1,11 +1,9 @@
 package com.ss.smartfilterlib.utils
 
-import androidx.appcompat.R
 import com.ss.smartfilterlib.data.MultiSelectionParams
 import com.ss.smartfilterlib.data.SingleChipSelectionParams
 import com.ss.smartfilterlib.data.SingleSelectionMultiRawParams
 import com.ss.smartfilterlib.data.SingleSelectionParams
-import com.ss.smartfilterlib.data.mRadioGroupData
 
 
 sealed class Params {
