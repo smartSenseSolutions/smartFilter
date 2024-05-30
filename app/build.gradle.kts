59e793cc3424df9df56a346f2c1cdd4be2fdf9f3
Mala Ruparel <Mala.Ruparel@smartsensesolutions.com>
2024-05-30 10:13:19 +0530

    remove test related file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,6 @@ android {
         versionCode = 1
         versionName = "1.0"
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -51,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":smartFilter"))
     implementation (libs.androidx.appcompat)
     implementation (libs.material)
     implementation(libs.androidx.core.ktx)
@@ -63,13 +63,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation.runtime.ktx)
     implementation(libs.androidx.navigation.compose)
-    implementation(project(":smartFilter"))
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
 
 }
